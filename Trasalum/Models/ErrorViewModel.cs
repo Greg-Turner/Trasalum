8b7a587f8038a467beccc015bd840f7062689788
Greg Turner <31353577+Greg-Turner@users.noreply.github.com>
2018-03-05 08:54:42 -0600

    Initial commit

---
+++ Trasalum/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Trasalum.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
